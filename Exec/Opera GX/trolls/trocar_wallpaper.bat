01459fe3437fb29dd672aace1b0d632e1cb9f515
FalcaoOtavio <otavioprogramador017@gmail.com>
2024-06-19 11:47:10 -0300

    first commit

--- Exec/Opera GX/trolls/trocar_wallpaper.bat
+++ Exec/Opera GX/trolls/trocar_wallpaper.bat
@@ -1,1 +1,12 @@
-start reg add HKCU\Software\Microsoft\CurrentVersion\Policies\System /v "wallpaper" /d wallpaper.jpg
+echo off
+
+:: Handle CLI Args
+IF [%1]==[] (
+echo No wallpaper path provided, please provide a full qualified path. Ex: C:\dir1\dir2\wallpaper.jpg
+exit /b 1
+)
+
+:: Commands
+echo Changing wallpaper to: %1
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %1 /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
